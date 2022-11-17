4d430daaa853c61bd339d9296b85d1c5e72c6e38
Jethro <galongau@gmail.com>
2022-11-17 21:57:26 +0000

    add skill search feature

--- components/SkillColumn.tsx
+++ components/SkillColumn.tsx
@@ -49,7 +49,7 @@ const SkillColumn = ({ title, items }: Props) => {
               className="h-[150px] w-full flex flex-col items-center justify-center"
             >
               <Image width={60} height={60} src={item.img} alt={item.name} />
-              <h4 className="mt-2 font-thin text-base lg:text-sm text-neutral-300 opacity-90">
+              <h4 className="mt-2 font-thin text-base lg:text-sm text-neutral-300 opacity-90 text-center">
                 {item.name}
               </h4>
             </motion.div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SkillColumn from "./SkillColumn";
 import { Item } from "../components/SkillColumn";
+import { BiSearch } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -21,35 +23,84 @@ const columns = [
   {
     title: "Frontend",
     items: [
-      { name: "ReactJS", img: "/image/react.svg" },
+      { name: "React", img: "/image/react.svg" },
       { name: "Redux", img: "/image/redux.svg" },
       { name: "NextJS", img: "/image/nextjs.svg" },
       { name: "SASS/SCSS", img: "/image/sass.svg" },
       { name: "TailwindCSS", img: "/image/tailwindcss.svg" },
+      { name: "Styled Components", img: "/image/styled-components.svg" },
       { name: "jQuery", img: "/image/jquery.svg" },
       { name: "GWT", img: "/image/gwt.svg" },
+      { name: "Bootstrap", img: "/image/bootstrap.svg" },
+      { name: "Material UI", img: "/image/mui.svg" },
     ],
   },
   {
     title: "Backend",
     items: [
+      { name: "NodeJS", img: "/image/nodejs.svg" },
       { name: "ExpressJS", img: "/image/express.svg" },
       { name: "Spring Boot", img: "/image/springboot.svg" },
-      { name: "NodeJS", img: "/image/nodejs.svg" },
+      { name: "JSP", img: "/image/jsp.svg" },
       { name: "MySQL", img: "/image/mysql.svg" },
       { name: "PostgreSQL", img: "/image/postgresql.svg" },
       { name: "MongoDB", img: "/image/mongodb.svg" },
       { name: "Firebase", img: "/image/firebase.svg" },
       { name: "GraphQL", img: "/image/graphql.svg" },
+      { name: "RESTful APIs", img: "/image/restapi.svg" },
     ],
   },
 ];
 
 const Skills = (props: Props) => {
+  const [isSearchFound, setIsSearchFound] = useState(false);
+  const searchRef = useRef<HTMLInputElement>(null);
+  const searchResultRef = useRef<HTMLParagraphElement>(null);
+
+  const handleSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (!searchRef || !searchRef.current || searchRef.current.value == "") {
+      return;
+    }
+
+    let skillList: string[] = [];
+    let targetStr = searchRef.current.value.toLowerCase().replaceAll(" ", "");
+
+    for (let i = 0; i < columns.length; i++) {
+      let currentColumn = columns[i];
+      for (let j = 0; j < currentColumn.items.length; j++) {
+        let name = currentColumn.items[j].name.toLowerCase();
+        name = name.replaceAll(" ", "");
+        skillList.push(name);
+      }
+    }
+
+    console.log({ skillList });
+
+    let isFound = false;
+    for (let i = 0; i < skillList.length; i++) {
+      console.log({ skill: skillList[i], targetStr });
+      if (targetStr.includes(skillList[i])) {
+        isFound = true;
+      }
+    }
+
+    setIsSearchFound(isFound);
+    searchResultRef.current?.classList.replace("opacity-0", "opacity-100");
+    console.log({ isSearchFound });
+
+    // setInterval(() => {
+    //   searchResultRef.current?.classList.replace("opacity-100", "opacity-0");
+    // }, 5000);
+  };
+
+  function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   return (
     <div id="skill-section" className="bg-black">
-      <div className="wrapper py-20 pb-40">
-        <div className="flex flex-col lg:flex-row gap-8">
+      <div className="wrapper py-20">
+        <div className="flex flex-col lg:flex-row gap-8 mb-10">
           {columns.map((column) => {
             return (
               <SkillColumn
@@ -60,6 +111,41 @@ const Skills = (props: Props) => {
             );
           })}
         </div>
+        <motion.div
+          initial={{ opacity: 0, translateY: 50 }}
+          whileInView={{
+            opacity: 1,
+            translateY: 0,
+            transition: { duration: 1 },
+          }}
+          className="text-center pt-20"
+        >
+          <h3 className="mb-4 text-lg font-thin">
+            Can't seem to find what you are looking for?
+          </h3>
+          <form className="flex gap-4 justify-center mb-4">
+            <input
+              ref={searchRef}
+              type="text"
+              placeholder="e.g. Java, React..."
+            />
+            <button
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
+              className="bg-green-700 hover:bg-green-600 transition-colors duration-200 px-3 rounded-lg"
+            >
+              <BiSearch />
+            </button>
+          </form>
+          <p
+            ref={searchResultRef}
+            className="transition-all duration-500 opacity-0"
+          >
+            {isSearchFound
+              ? 'It is right in my "toolbox"! I might be the developer you are looking for...'
+              : "Unfontunately, I don't have the skill you are looking for, at the moment. As a professional software engineer, I love challenges and always love to learn new things!"}
+          </p>
+        </motion.div>
       </div>
     </div>
   );
