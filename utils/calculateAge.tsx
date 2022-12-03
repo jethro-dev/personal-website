b3b56770a389226280e266d87f4a950e07e40805
Jethro <galongau@gmail.com>
2022-12-03 20:19:38 +0000

    add function to calculate age

---
+++ utils/calculateAge.tsx
@@ -0,0 +1,8 @@
+function calculate_age(dob: Date) {
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms);
+
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+export default calculate_age;
