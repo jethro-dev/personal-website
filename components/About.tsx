ff6a1580b62dc750edcb2bbc11e8f412b368ef4f
Jethro <galongau@gmail.com>
2022-12-03 20:18:11 +0000

    refactor about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,48 +1,88 @@
+import Image from "next/image";
 import React from "react";
-import useConfettiGradient from "../hooks/useConfettiGradient";
-import useWindowDimensions from "../hooks/useWindowDimensions";
-import StaggeredGrid from "./StaggeredGrid";
-import { FaArrowDown } from "react-icons/fa";
-import scrollToById from "../utils/scrollToById";
+import calculateAge from "../utils/calculateAge";
+import { FiClipboard } from "react-icons/fi";
 type Props = {};
 
-export const SECTION_HEIGHT = 800;
-
 const About = (props: Props) => {
-  const { ConfettiGradient } = useConfettiGradient(1.5);
-  const { height, width } = useWindowDimensions();
+  const handleCopy = () => {
+    navigator.clipboard.writeText("galongau@gmail.com");
+  };
 
   return (
-    <div
-      id="about-section"
-      className={`relative h-[${SECTION_HEIGHT}px]`}
-      style={{ height: height > SECTION_HEIGHT ? height : SECTION_HEIGHT }}
-    >
-      <ConfettiGradient />
-      <StaggeredGrid
-        height={height > SECTION_HEIGHT ? height : SECTION_HEIGHT}
-        width={width}
-      />
-      <div className="px-6 lg:px-10 absolute z-10 h-full w-full pointer-events-none bg-gradient">
-        <div className="wrapper h-full grid place-items-center">
-          <div>
-            <h1 className="text-7xl font-semibold mb-2">Hello, I am Jethro.</h1>
-            <h2 className="text-3xl font-light mb-4">A Software Engineer</h2>
-            <p className="text-xl font-light mb-20">
-              High level experience in software engineering and development,
-              producing quality work.
+    <div id="skill-section" className="bg-black">
+      <div className="wrapper py-20 flex flex-col md:flex-row gap-8">
+        {/* left */}
+        <div className="image-container min-h-[600px] md:w-[50%] relative rounded-lg overflow-hidden">
+          <Image
+            src="/image/profile_picture.jpg"
+            layout="fill"
+            alt="Jethro Au"
+            className="object-center object-cover"
+            id="profile_picture"
+          />
+        </div>
+        {/* right */}
+        <div className="md:w-[50%] flex flex-col gap-5">
+          <div className="rounded-md bg-neutral-800 bg-opacity-50 full-glassify p-5 relative">
+            <p className="mb-2">
+              <span className="font-semibold">Name: </span>
+              <span className="font-thin">Jethro Au</span>
             </p>
-            <div className="grid place-items-center absolute left-[50%] translate-x-[-50%]">
-              <h3 className="mb-3 text-neutral-300 opacity-90 font-medium">
-                click to see more
-              </h3>
-              <button
-                onClick={() => scrollToById("skill-section")}
-                className="full-glassify font-semibold text-3xl p-4 rounded-full pointer-events-auto !border-2 hover:scale-105 transition-all duration-500"
-              >
-                <FaArrowDown />
-              </button>
-            </div>
+            <p className="mb-2">
+              <span className="font-semibold">Email: </span>
+              <span className="font-thin relative" id="myEmail">
+                galongau@gmail.com
+                <button
+                  className="rounded-sm absolute right-[-25px] top-[50%] translate-y-[-50%] text-violet-500 hover:scale-105 transition-all"
+                  title="copy to clipboard"
+                  onClick={() => handleCopy()}
+                >
+                  <FiClipboard size={20} />
+                </button>
+              </span>
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Location: </span>
+              <span className="font-thin">London</span>
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Age: </span>
+              <span className="font-thin">
+                {calculateAge(new Date(1999, 9, 6))}
+              </span>
+            </p>
+            <p className="mb-4 lg:mb-0">
+              <span className="font-semibold">Occupation: </span>
+              <span className="font-thin">Software Engineer</span>
+            </p>
+            <button className="px-4 py-2 ring-2 ring-inset ring-violet-500 text-violet-500 rounded-md lg:absolute top-5 right-5 font-semibold hover:bg-violet-500 hover:text-black transition-all duration-500 ">
+              Download CV
+            </button>
+          </div>
+          <div className="ring-2 rounded-md flex-1 bg-neutral-800 bg-opacity-50 full-glassify p-5">
+            <h1 className="text-xl font-semibold mb-2">What about me?</h1>
+            <p className="font-thin mb-5">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              dolore dolorum repellendus dolores dolorem molestias sit explicabo
+              placeat facilis consectetur?
+            </p>
+            <h1 className="text-xl font-semibold mb-2">
+              Why do I love software development?
+            </h1>
+            <p className="font-thin mb-5">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              dolore dolorum repellendus dolores dolorem molestias sit explicabo
+              placeat facilis consectetur?
+            </p>
+            <h1 className="text-xl font-semibold mb-2">
+              What makes me a good software engineer?
+            </h1>
+            <p className="font-thin">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              dolore dolorum repellendus dolores dolorem molestias sit explicabo
+              placeat facilis consectetur?
+            </p>
           </div>
         </div>
       </div>
