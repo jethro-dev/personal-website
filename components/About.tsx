6f1198a5d032d9fd460483c78f4be97d844b019d
Jethro <galongau@gmail.com>
2022-12-05 01:50:46 +0000

    change text color and content

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import calculateAge from "../utils/calculateAge";
 import { FiClipboard } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 type Props = {};
 
 const About = (props: Props) => {
@@ -27,14 +28,17 @@ const About = (props: Props) => {
           <div className="rounded-md bg-neutral-800 bg-opacity-50 full-glassify p-5 relative">
             <p className="mb-2">
               <span className="font-semibold">Name: </span>
-              <span className="font-thin">Jethro Au</span>
+              <span className="font-thin text-neutral-300">Jethro Au</span>
             </p>
             <p className="mb-2">
               <span className="font-semibold">Email: </span>
-              <span className="font-thin relative" id="myEmail">
+              <span
+                className="font-thin relative text-neutral-300"
+                id="myEmail"
+              >
                 galongau@gmail.com
                 <button
-                  className="rounded-sm absolute right-[-25px] top-[50%] translate-y-[-50%] text-violet-500 hover:scale-105 transition-all"
+                  className="rounded-sm absolute right-[-25px] top-[50%] translate-y-[-50%] text-violet-500 hover:text-violet-600 transition-all"
                   title="copy to clipboard"
                   onClick={() => handleCopy()}
                 >
@@ -44,44 +48,77 @@ const About = (props: Props) => {
             </p>
             <p className="mb-2">
               <span className="font-semibold">Location: </span>
-              <span className="font-thin">London</span>
+              <span className="font-thin text-neutral-300">London</span>
             </p>
             <p className="mb-2">
               <span className="font-semibold">Age: </span>
-              <span className="font-thin">
+              <span className="font-thin text-neutral-300">
                 {calculateAge(new Date(1999, 9, 6))}
               </span>
             </p>
             <p className="mb-4 lg:mb-0">
               <span className="font-semibold">Occupation: </span>
-              <span className="font-thin">Software Engineer</span>
+              <span className="font-thin text-neutral-300">
+                Software Engineer
+              </span>
             </p>
             <button className="px-4 py-2 ring-2 ring-inset ring-violet-500 text-violet-500 rounded-md lg:absolute top-5 right-5 font-semibold hover:bg-violet-500 hover:text-black transition-all duration-500 ">
               Download CV
             </button>
+            <div className="absolute bottom-5 right-5">
+              <ul className="flex items-center gap-4 text-violet-500 ">
+                {/* linkedin */}
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/galongau/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:text-violet-600 transition-all duration-500"
+                  >
+                    <FaLinkedin size={28} />
+                  </a>
+                </li>
+                {/* github */}
+                <li>
+                  <a
+                    href="https://github.com/jethro-dev"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:text-violet-600 transition-all duration-500"
+                  >
+                    <FaGithub size={28} />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="ring-2 rounded-md flex-1 bg-neutral-800 bg-opacity-50 full-glassify p-5">
             <h1 className="text-xl font-semibold mb-2">What about me?</h1>
-            <p className="font-thin mb-5">
+            <p className="font-thin mb-5 text-neutral-300">
               A passionate STEM graduate following his dreams. Originated in
-              Hong Kong, currently living in the capital of UK — London. This
-              gives me the ability to read and write fluently in two of the most
-              spoken languages in the world, English and Chinese.
+              Hong Kong, currently living in the capital of the UK — London.
+              Giving me the ability to read and write fluently in two of the
+              most spoken languages in the world, English, and Chinese.
             </p>
             <h1 className="text-xl font-semibold mb-2">
-              Why do I love software development?
+              Why do I love software development so much?
             </h1>
-            <p className="font-thin mb-5">
-              I love to create. I am curious. I love computer and technology. I
-              love to create products to help people acoss the globe.
+            <p className="font-thin mb-5 text-neutral-300">
+              There's nothing more satisfying than solving a problem that's been
+              around for a while and nobody else knows how to solve. As a
+              Software Developer you constantly provide solutions for users'
+              problems. You can be working on the occasional quick fix as well
+              as more complex strategic solutions.
             </p>
             <h1 className="text-xl font-semibold mb-2">
               What makes me a good software engineer?
             </h1>
-            <p className="font-thin">
-              I love challenges. I have great attitude and never give up.
-              Ability to read docs across the world. Always curious. Always stay
-              updated. Always eager to learn new technologies.
+            <p className="font-thin text-neutral-300">
+              Great fundamentals built up from university. I am always curious
+              and love to stay updated to the latest technology. Also, I love
+              challenges and I don't give up. With the advantages of bilingual,
+              I am able to read documentations across the globe. Giving me the
+              opportunity to learn things from different perspective.
             </p>
           </div>
         </div>
