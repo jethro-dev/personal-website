bedf6fe448ef6bbd4eb3f222ac171ace36af5b5f
Jethro <jethro@livr.co>
2024-01-30 00:22:14 +0000

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
@@ -7,33 +6,11 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { poppins } from "./fonts";
 
-const metadata: Metadata = {
-  title: "jethroau.com",
+export const metadata: Metadata = {
+  title: "Jethro Au",
   description:
     "Explore the intersection of technology and imagination as we embark on a journey to transform concepts into powerful, user-centric experiences. Let's build the future together – one line of code at a time.",
   keywords: ["jethro", "au", "jethroau", "jethroau.com", "personal", "website"],
-  // openGraph: {
-  //   title: "Jethro Au Personal Website",
-  //   description:
-  //     "Explore the intersection of technology and imagination as we embark on a journey to transform concepts into powerful, user-centric experiences. Let's build the future together – one line of code at a time.",
-  //   url: "https://jethroau.com",
-  //   siteName: "jethroau.com",
-  //   images: [
-  //     {
-  //       url: "https://nextjs.org/og.png", // Must be an absolute URL
-  //       width: 800,
-  //       height: 600,
-  //     },
-  //     {
-  //       url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
-  //       width: 1800,
-  //       height: 1600,
-  //       alt: "My custom alt",
-  //     },
-  //   ],
-  //   locale: "en_US",
-  //   type: "website",
-  // },
 };
 
 export default function RootLayout({
