d2fe72fa2436d74fdf91b28309e52bc0d1105efa
Jethro <jethro@livr.co>
2024-02-08 05:24:07 +0000

    embed studio

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,9 +1,24 @@
-"use client";
+// ./src/app/studio/[[...index]]/page.tsx
+import type { Metadata } from "next";
+import type { Viewport } from "next";
+import { metadata as studioMetadata } from "next-sanity/studio/metadata";
+import { viewport as studioViewport } from "next-sanity/studio/viewport";
 
-import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity/sanity.config";
+import { Studio } from "./Studio";
 
-export default function Studio() {
-  /*@ts-ignore */
-  return <NextStudio config={config} />;
+// Set the right `viewport`, `robots` and `referer` meta tags
+export const metadata: Metadata = {
+  ...studioMetadata,
+  // Overrides the title until the Studio is loaded
+  title: "Loading Studio…",
+};
+
+export const viewport: Viewport = {
+  ...studioViewport,
+  // Overrides the viewport to resize behavior
+  interactiveWidget: "resizes-content",
+};
+
+export default function StudioPage() {
+  return <Studio />;
 }
---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+// ./sanity.config.ts
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+
+import { schemaTypes } from "@/schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio", // <-- important that `basePath` matches the route you're mounting your studio from
+
+  projectId,
+  dataset,
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+});
