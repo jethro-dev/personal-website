0b9caaf519e2170cae3f9a06b0be87d1c8833bc1
Jethro <jethro@livr.co>
2024-02-10 20:46:33 +0000

    update path

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,8 +2,7 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
-
-import { schemaTypes } from "@/sanity/schemas";
+import { schemaTypes } from "./sanity/schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
